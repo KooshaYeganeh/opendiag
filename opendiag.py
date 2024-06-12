65dfa5ea4698e37ed8ae3ee6c6a1b1ea43d415c5
KooshaYeganeh <kooshakooshadv@gmail.com>
2024-06-13 01:29:52 +0330

    add new Modules

--- opendiag.py
+++ opendiag.py
@@ -1,21 +1,21 @@
-import os
+# opendiag.py
+#!/usr/bin/env python
+
 import sys
-import obd
 import parameters
-import dtc
 import help
-from colorama import Fore, Back, Style
-
-
+from colorama import Fore
+from dtc import check_dtc  # Import check_dtc from dtc module
+from can_sniffer import start_can_sniffer  # Import start_can_sniffer from can_sniffer module
+import oscilloscope  # Import oscilloscope module
 
 print(Fore.LIGHTGREEN_EX + """
-
             ██╗  ██╗██╗   ██╗ ██████╗ ███╗   ██╗██╗   ██╗███████╗
             ██║ ██╔╝╚██╗ ██╔╝██╔════╝ ████╗  ██║██║   ██║██╔════╝
             █████╔╝  ╚████╔╝ ██║  ███╗██╔██╗ ██║██║   ██║███████╗
             ██╔═██╗   ╚██╔╝  ██║   ██║██║╚██╗██║██║   ██║╚════██║
             ██║  ██╗   ██║   ╚██████╔╝██║ ╚████║╚██████╔╝███████║
-            ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+            ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚══════╝
 
              ██████╗ ██████╗ ███████╗███╗   ██╗██████╗ ██╗ █████╗  ██████╗
             ██╔═══██╗██╔══██╗██╔════╝████╗  ██║██╔══██╗██║██╔══██╗██╔════╝
@@ -23,22 +23,29 @@ print(Fore.LIGHTGREEN_EX + """
             ██║   ██║██╔═══╝ ██╔══╝  ██║╚██╗██║██║  ██║██║██╔══██║██║   ██║
             ╚██████╔╝██║     ███████╗██║ ╚████║██████╔╝██║██║  ██║╚██████╔╝
              ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝  ╚═╝ ╚═════╝
+      """ + Fore.RESET)
 
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python opendiag.py <command>")
+        sys.exit(1)
 
+    command = sys.argv[1]
 
-      """)
-
-
-
-
-if sys.argv[0] == "opendiag.py":
-    if sys.argv[1] == "--dtc" or sys.argv[1] == "--D":
-        dtc.check_dtc()
-    elif sys.argv[1] == "--parameters" or sys.argv[1] == "-PR":
+    if command == "--dtc" or command == "--D":
+        print(check_dtc())
+    elif command == "--parameters" or command == "-PR":
         parameters.para()
-    elif sys.argv[1] == "--help" or sys.argv[1] == "-H":
+    elif command == "--can-sniffer" or command == "--CS":
+        start_can_sniffer()
+    elif command == "--oscilloscope" or command == "--O":
+        oscilloscope.start_obd_plotting()
+    elif command == "--help" or command == "H":
         help.manual()
     else:
-        print(Fore.LIGHTGREEN_EX + "> check command and Try Again" + Fore.RESET)
-        print(Fore. LIGHTRED_EX + "> For More Informatin check DTC command info or command opendiag --help" + Fore.RESET)
+        print(Fore.LIGHTRED_EX + "> Invalid command. Please check your command and try again." + Fore.RESET)
+        print(Fore.LIGHTRED_EX + "> For more information, use 'opendiag --help'." + Fore.RESET)
+
+if __name__ == "__main__":
+    main()
 
